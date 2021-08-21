ba60814fcae10c7faa54804b50a60e3ba32254a9
wangkui0508 <wangkui0508@hotmail.com>
2021-08-21 10:17:44 +0800

    Fix a typo

--- contracts/xhedge.sol
+++ contracts/xhedge.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface PriceOracle {
 	function getPrice() external returns (uint);
